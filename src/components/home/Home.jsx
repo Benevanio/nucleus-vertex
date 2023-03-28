0776ea906e27b36f77676351b9d5e816c8519841
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-28 18:17:30 -0300

    Formatando o documento de maneira adequada e visualmente aceitavel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,29 +7,29 @@ import Footer from '../Footer/Footer'
 const Home = () => {
   return (
     <>
-    <Navbar />
-    <section className="home">
-      <div className="home__container">
-        <h1 className="home__title">Welcome to nucleus vertex</h1>
-        <p className="home__description">
-          O nucleus vertex é um projeto de código aberto que utiliza a API do
-          News API para exibir as últimas notícias do mundo.
-        </p>
-        <p className="home__description">
-          O projeto foi desenvolvido com ReactJS, Bootstrap e Sass.
-        </p>
+      <Navbar />
+      <section className="home">
+        <div className="home__container">
+          <h1 className="home__title">Welcome to nucleus vertex</h1>
+          <p className="home__description">
+            O nucleus vertex é um projeto de código aberto que utiliza a API do
+            News API para exibir as últimas notícias do mundo.
+          </p>
+          <p className="home__description">
+            O projeto foi desenvolvido com ReactJS, Bootstrap e Sass.
+          </p>
 
-      </div>
-      <div className="Section_api__container">
-        <button className="btn btn-primary">
-          <Link className="link" to="/api">
-            Acessar a API
-          <i className="fas fa-arrow-right"></i>
-           </Link>
-        </button>
-      </div>
-    </section>    
-   <Footer />
+        </div>
+        <div className="Section_api__container">
+          <button className="btn btn-primary">
+            <Link className="link" to="/api">
+              Acessar a API
+              <i className="fas fa-arrow-right"></i>
+            </Link>
+          </button>
+        </div>
+      </section>
+      <Footer />
     </>
   )
 }
