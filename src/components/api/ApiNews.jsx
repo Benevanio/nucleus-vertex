88cd94cee9fe6df3a7daafcfd5d1a008a7a731f2
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-28 18:18:02 -0300

    Atualizando a news api

--- src/components/api/ApiNews.jsx
+++ src/components/api/ApiNews.jsx
@@ -1,66 +1,66 @@
 import React from 'react'
 import './ApiNews.scss'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Footer from '../Footer/Footer';
 import Navbar from '../navbar/Navbar';
 const Api = () => {
-    const APIURNEWSAPI = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=85b932b146bd4418943f0e62c98f976a';
-    const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const APIURNEWSAPI = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=85b932b146bd4418943f0e62c98f976a';
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchArticles = async () => {
-            const response = await fetch(APIURNEWSAPI);
-            const data = await response.json();      
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await fetch(APIURNEWSAPI);
+      const data = await response.json();
 
-            setArticles(data.articles);
-            setLoading(false);
-          
-        }
-        fetchArticles();
-    }, [])
-   
-    if (loading) {
-        return    <div className="honeycomb">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      setArticles(data.articles);
+      setLoading(false);
+
     }
+    fetchArticles();
+  }, [])
+
+  if (loading) {
+    return <div className="honeycomb">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  }
   return (
     <>
-    <Navbar />
-    <div className="container container2">
+      <Navbar />
+      <div className="container container2">
         <div className="row">
-        {articles.map((article, id ) => (
-          <div className="col-sm-6 mb-3 mb-sm-0" key={id}>
-            <div className="card mb-5 shadow-sm">
-            <div className="card">
-              <div className="card-body">
-              <h5 className="card-title">{article.author}</h5>
-              <p className="card-text">{article.content}</p>   
-              <p className="card-text">{article.description}</p>
-              <p className="card-text">{article.publishedAt}</p>
-              <p className="card-text">{article.source.name}</p>
-              <p className="card-text">{article.title}</p>
-              <a href={article.url} target="_blank" rel="noreferrer" className="card-text">
-              Veja a materia completa em..
-              <i className="fas fa-external-link-alt"></i>
-              </a>
-              <img src={article.urlToImage} alt="IMAGEN DA NOTICIA" className="card-img-top" />
+          {articles.map((article, id) => (
+            <div className="col-sm-6 mb-3 mb-sm-0" key={id}>
+              <div className="card mb-5 shadow-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{article.author}</h5>
+                    <p className="card-text">{article.content}</p>
+                    <p className="card-text">{article.description}</p>
+                    <p className="card-text">{article.publishedAt}</p>
+                    <p className="card-text">{article.source.name}</p>
+                    <p className="card-text">{article.title}</p>
+                    <a href={article.url} target="_blank" rel="noreferrer" className="card-text">
+                      Veja a materia completa em..
+                      <i className="fas fa-external-link-alt"></i>
+                    </a>
+                    <img src={article.urlToImage} alt="IMAGEN DA NOTICIA" className="card-img-top" />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        ))}
+          ))}
         </div>
 
-   </div>
-   <Footer />
+      </div>
+      <Footer />
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,32 +6,32 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Nucleus <span className="text-primary">Vertex</span>
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="/">
-            <i className="fas fa-home"></i>
-            </a>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link" to="/api">
-            <i className="fas fa-newspaper"></i>
-            </Link>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Nucleus <span className="text-primary">Vertex</span>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">
+                <i className="fas fa-home"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/api">
+                <i className="fas fa-newspaper"></i>
+              </Link>
 
-        </li>
-        </ul>
+            </li>
+          </ul>
 
-       
-    </div>
-  </div>
-</nav>
+
+        </div>
+      </div>
+    </nav>
   )
 }
 
